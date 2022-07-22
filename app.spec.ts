fd32e7fed5e779e91de874c8e5d8d72f50abb6c0
Jiří Špác <capajj@gmail.com>
2022-07-22 13:54:35 +0200

    add one more spec

--- app.spec.ts
+++ app.spec.ts
@@ -1,8 +1,15 @@
 import { assert, describe, expect, it } from 'vitest'
 import { gqlSchema } from './gqlSchema'
+import { startupDB } from './startupDb'
 
 describe('suite name', () => {
   it('foo', () => {
     expect(gqlSchema).to.be.toBeTruthy()
+  })
+
+  it('should aa', async () => {
+    startupDB[0].addStage('New stage')
+
+    expect(startupDB[0].stages[3].title).to.be.toBe('New stage')
   })
 })
